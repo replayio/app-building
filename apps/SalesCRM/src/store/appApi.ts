8fa6093f8b367b0cedd2f924e50e16124078be23
App Builder <app-builder@localhost>
2026-02-23 19:57:37 +0000

    Remove apps/ and logs/ before merging to main

--- apps/SalesCRM/src/store/appApi.ts
+++
@@ -1,8 +0,0 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-
-export const appApi = createApi({
-  reducerPath: 'appApi',
-  baseQuery: fetchBaseQuery({ baseUrl: '/.netlify/functions/' }),
-  tagTypes: ['UserInfo'],
-  endpoints: () => ({}),
-})
